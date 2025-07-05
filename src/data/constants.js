62a6d5ded5afcd02f621d5717d42f7615ceb0615
Ajith5104 <ajithkumar51046@gmail.com>
2025-07-06 00:25:50 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,210 @@
+export const Bio = {
+  name: "Priyadharsani V",
+  HeroImg: "images/heroImage.png",
+  roles: ["3D Artist", "Graphic Designer"],
+  description:
+    "Creative and detail-oriented 3D Artist & Graphic Designer skilled in modeling, texturing, and visual storytelling. Passionate about bringing ideas to life through 3D art and compelling designs. Seeking an opportunity to contribute my technical and artistic skills to innovative projects while growing in a collaborative environment.",
+  github: "https://github.com/Ajith5104",
+  resume:
+    "https://drive.google.com/file/d/1-C7N0wz31n4rmn8qeu2wOD6CConIwx_9/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ajithkumar22/",
+  email: "mailto:ajithkumar51046@gmail.com",
+  insta: "https://www.instagram.com/ajithtechy22/",
+  phone: "tel: +91 8610216706",
+  whatsapp: 8610216706,
+};
+
+export const skills = [
+  {
+    title: "3D Modeling & Animation",
+    skills: [
+      {
+        name: "Autodesk Maya",
+        image:
+          "https://images.seeklogo.com/logo-png/30/1/autodesk-maya-logo-png_seeklogo-308077.png",
+      },
+    ],
+  },
+  {
+    title: "Game Designing",
+    skills: [
+      {
+        name: "Unity Hub ",
+        image: "https://img.icons8.com/nolan/512/unity-hub.png",
+      },
+    ],
+  },
+  {
+    title: "Graphic Design",
+    skills: [
+            {
+        name: "Illustrator",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/013/403/705/non_2x/adobe-illustrator-logo-free-vector.jpg",
+      },
+           {
+        name: "Photoshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+      },
+      {
+        name: "CorelDRAW",
+        image:
+          "https://e7.pngegg.com/pngimages/561/836/png-clipart-coreldraw-computer-software-graphics-suite-others-miscellaneous-cdr-thumbnail.png",
+      },
+
+
+    ],
+  },
+
+  {
+    title: "2D Animation",
+    skills: [
+            {
+        name: "After Effects",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Adobe_After_Effects_CC_icon.svg/1200px-Adobe_After_Effects_CC_icon.svg.png",
+      },
+ 
+      {
+        name: "Adobe Animate",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Adobe_Animate_CC_icon_%282020%29.svg/2097px-Adobe_Animate_CC_icon_%282020%29.svg.png",
+      },
+
+    ],
+  },
+  {
+    title: "UI & UX Design",
+    skills: [
+      {
+        name: "Figma",
+        image:
+          "https://cdn.prod.website-files.com/5f15081919fdf673994ab5fd/63feaf1bf551685b4ac7dd7b_Fima%20Logo.svg",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf-ShdTs4jJCfS622sm65i-VqxC_a_Eg738A&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMZv_SgNc0NGgQUYYy4QLGiVWrjW04DSVK-w&s",
+    role: "Drawing Teacher",
+    company: "Ms Art Academy",
+    date: "2022 - 2025",
+    desc: "Passionate part-time drawing teacher with a creative approach to inspiring students of all ages. Dedicated to fostering artistic skills and confidence through engaging, hands-on lessons.",
+    // skills: [
+    //   "React",
+    //   "JavScript",
+    //   "NodeJS",
+    //   "ExpressJs",
+    //   "HTML5",
+    //   "CSS3",
+    //   "Bootstrap",
+    //   "MSSQL",
+    //   "MongoDB",
+    //   "Git",
+    //   "GitHub",
+    // ],
+    // credentials: "images/mern.jpg",
+  },
+  // {
+  //   id: 1,
+
+  //   img: "https://media.licdn.com/dms/image/C510BAQEmJOdwPERyXA/company-logo_200_200/0/1630581621973/shiash_info_solutions_private_limited_logo?e=2147483647&v=beta&t=b_ZA_THGpWEuyLVKvAd32tXGN0o1rBjgbBsmMOtykrQ",
+  //   role: "Python Developer Intern",
+  //   company: "Shiash Info Solutions Private Limited",
+  //   date: "Apr 2023 - Jun 2023",
+  //   desc: "During my Python Developer internship, I excelled in day-to-day tasks while absorbing new technologies and gaining practical insights into real-world project execution and software life cycles. Interactions with senior developers provided valuable mentorship and firsthand industry experience, which greatly enhanced my proficiency and understanding of professional software development practices.",
+  //   skills: ["Python", "Git", "GitHub", "SQL"],
+  //   credentials: "images/shiash.jpg",
+  //   cer: "Certificate",
+  // },
+];
+export const education = [
+  {
+    id: 1,
+
+    school: "Loyola College",
+    date: "2023-2026",
+    grade: "Pursuing",
+    desc: "",
+    degree: "Bachelor of Vocational Studies 3D Animation",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GreensCart",
+    //date: "2023 - 2024",
+    // description: "Discover a modern e-commerce experience with our platform developed using the MERN (MongoDB, Express.js, React.js, Node.js) stack. Our frontend is designed to be responsive and user-friendly, utilizing Bootstrap for seamless navigation across devices. Complete your purchases securely using the Stripe API, ensuring smooth and reliable payment processing. Stay informed with real-time email notifications powered by the Mailtrap API, keeping you updated on orders and promotions. Explore a wide range of products with ease, thanks to our intuitive interface and dynamic features. With cutting-edge technology and a focus on user experience, our e-commerce website offers a streamlined shopping journey tailored to your needs.",
+    image: "images/projectimg/home.jpg",
+    // tags: [
+    //   "React",
+    //   "NodeJS",
+    //   "ExpressJS",
+    //   "MongoDB",
+    //   "Bootstrap",
+    //   "Stripe API",
+    //   "Mailtrap API",
+    // ],
+    // category: "webApp",
+    // github: "https://github.com/Ajith5104/greensCart",
+    // webapp: "https://github.com/Ajith5104/greensCart",
+  },
+  {
+    id: 1,
+    title: "Weather-Forecasting",
+    // date: "2023",
+  // description:"Designed and developed a Tourism Web Application, offering seamless trip planning, booking, and exploration experiences for travelers.",
+    image: "images/projectimg/weather.JPEG",
+    // tags: ["HTML", "CSS", "JavaScript", "Openweathermap API"],
+    // category: "webApp",
+    // github: "https://github.com/Ajith5104/Weather-Forecasting-Web-App",
+    // webapp: "https://weather-live-application.netlify.app/",
+  },
+   {
+    id: 2,
+    title: "Organicmart",
+    // date: "2023",
+    // description: "Welcome to our online shopping website crafted with HTML, CSS, and Bootstrap, offering a sleek and responsive design for effortless browsing. Experience seamless form submissions and communication using Web3's email service API, ensuring efficient customer interaction. Explore a curated selection of products across various categories, all presented in an intuitive layout designed for optimal user experience.",
+    image: "images/projectimg/organicmart.JPEG",
+    // tags: ["HTML", "CSS", "Bootstrap", "Email Service API"],
+    // category: "website",
+    // github: "https://github.com/Ajith5104/Organic_Mart",
+    // webapp: "https://organicmart.netlify.app/",
+   },
+  // {
+  //   id: 3,
+  //   title: "Tourism-Website",
+  //   date: "2023",
+  //   description:
+  //     "Embark on your next adventure with our tourism website featuring HTML, CSS, JavaScript, Bootstrap, PHP, and MySQL technologies. Explore captivating destinations and plan your itinerary with interactive maps and rich multimedia content. Engage in real-time assistance and personalized recommendations through our Tawk chatbot integration, ensuring a seamless and informative experience. Discover, plan, and book your dream vacation effortlessly on our user-friendly tourism platform.",
+  //   image: "images/projectimg/tatabyebye.jpg",
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "javaScript",
+  //     "Bootstrap",
+  //     "PHP",
+  //     "MySQL",
+  //     "Tawk chatbot   API ",
+  //   ],
+  //   category: "website",
+  //   github: "https://github.com/Ajith5104/Tourism-Website",
+  //   webapp: "https://github.com/Ajith5104/Tourism-Website/",
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2026, text: "Loyola College" },
+  { year: 2026, text: "Bachelor of Vocational Studies 3D Animation" },
+  { year: 2026, text: "Started my career as 3D Artist & Graphic Designer" },
+];
