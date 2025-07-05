e9d9da092a40e93fd01e60216b6f8216917927bc
Ajith5104 <ajithkumar51046@gmail.com>
2025-07-06 00:39:03 +0530

    first commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -23,7 +23,7 @@ const Projects = ({ openModal, setOpenModal }) => {
           I have worked on a wide range of projects. From websites to web apps.
           Here are some of my projects.
         </Desc>
-        <ToggleButtonGroup>
+        {/* <ToggleButtonGroup>
           {toggle === "all" ? (
             <ToggleButton active value="all" onClick={() => setToggle("all")}>
               All
@@ -62,7 +62,7 @@ const Projects = ({ openModal, setOpenModal }) => {
               WEBSITE
             </ToggleButton>
           )}
-        </ToggleButtonGroup>
+        </ToggleButtonGroup> */}
         <CardContainer>
           {toggle === "all" &&
             projects.map((project) => (
